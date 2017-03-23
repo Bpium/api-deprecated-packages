2918da6680b9fd5fa784b4c57e0b6f67206de871
Ramil Garipov <ramillion@ya.ru>
2017-03-23 11:16:53 +0300

    initial commit

--- generate.js
+++ generate.js
@@ -1,13 +1,9 @@
-const exec = require('child_process').exec;
+const fs = require('fs');
 const packages = require('./package.json');
 
-const command = Object.keys(packages.dependencies).map(packageName=> {
-  return `echo "module.exports = require('${packageName}')" > packages/${packageName}.js`;
-}).join(' && ');
-
-exec(command, err=> {
-  if (err) {
-    console.error('Generate deprecated packages err: ', err);
-    process.abort();
-  }
-});
+Object.keys(packages.dependencies).map(packageName=> {
+  fs.writeFileSync(
+    `packages/${packageName}.js`, 
+    `module.exports = require('${packageName}')`
+  );
+})
